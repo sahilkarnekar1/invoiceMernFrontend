b5a12d6163049ee3759b123fe41f30f4813e642f
sahilkarnekar1 <sahilkarnekar1@gmail.com>
2024-06-26 18:03:33 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://invoice-mern-backend.vercel.app/api';
 
 export const createSeller = async (seller) => {
   return await axios.post(`${API_URL}/sellers`, seller);
